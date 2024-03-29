767ff7dd010bc9bcf7b057d336e414508003d522
lego7set <84934297+lego7set@users.noreply.github.com>
2024-03-29 15:32:23 -0400

    make it so that penguinscript is sorta an extension???

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,23 @@ import type { TokenType, TokenList } from "./parsing/lexer.ts";
 
 import Lexer, { Token } from "./parsing/lexer";
 
+class PenguinScript {
+  
+}
+
+if (typeof Scratch === "object" && Scratch && window && typeof window === "object" && typeof window.document === "object") {
+  // Logic here
+}
+
+
 console.log(Lexer, Token)
 
 module.exports = {
-  e: ()=>Lexer
+  e: ()=>Lexer,
+  PenguinScript,
+  default: function transpile(src): string | Function {
+    //
+    return src;
+  }
 }
+          
