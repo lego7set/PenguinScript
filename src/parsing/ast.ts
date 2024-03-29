6a3afaf377c2bbc8241c78b02f089b9a96599578
lego7set <84934297+lego7set@users.noreply.github.com>
2024-03-29 18:19:55 -0400

    create type definitions for ast

---
+++ src/parsing/ast.ts
@@ -0,0 +1,60 @@
+export enum NodeType {
+  Program,
+  VariableDeclaration,
+  AssignmentExpr,
+  Identifier,
+  PrimitiveLiteral
+}
+
+export enum OutputType { // going to be unused
+  String,
+  Number,
+  Boolean,
+  Object,
+  List,
+  VariableReference,
+  Userdata
+}
+
+export interface Stmt {
+  kind: NodeType;
+}
+
+export type StmtBody = Stmt[];
+
+export interface Program extends Stmt {
+  kind: NodeType.Program;
+  body: StmtBody;
+}
+
+export interface VariableDeclaration extends Stmt {
+  kind: NodeType.VariableDeclaration;
+  constant: boolean;
+  symbol: string;
+  value?: Expr;
+}
+
+export interface Expr extends Stmt {};
+
+export interface AssignmentExpr extends Expr {
+  kind: NodeType.AssignmentExpr;
+  assigne: Expr;
+  value: Expr;
+}
+
+export interface Identifer extends Expr {
+  kind: NodeType.Identifier;
+}
+
+export interface PrimitiveLiteral {
+  kind: NodeType.PrimitiveLiteral,
+  value: number | string | boolean | null
+}
+
+export interface NumericLiteral extends PrimitiveLiteral {
+  value: number;
+}
+
+export interface StringLiteral extends PrimitiveLiteral {
+  value: string;
+}
