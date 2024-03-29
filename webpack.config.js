0c27ce0862b1fae985f1f5fb2aea2f5d801d9732
lego7set <84934297+lego7set@users.noreply.github.com>
2024-03-29 10:23:03 -0400

    create webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = {
+  entry: "/src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    path: "/psout",
+    filename: 'main.js',
+    libraryTarget: 'umd'
+  }
+}
