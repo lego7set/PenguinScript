1a6bbdbc48616d21533f69d7e17f61695cf0d18c
lego7set <84934297+lego7set@users.noreply.github.com>
2024-03-30 09:46:31 -0400

    update index.ts to see whats wrong

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import Parser from "./parsing/parser";
 
+import Lexer from "./parsing/lexer";
+
+const xyz = new Lexer("let x = 5; x = x or 5 and 3 or not 7").tokenizeNonGen()
+console.log(xyz);
+
+debugger;
+
 console.log(new Parser("let x = 5; x = x xor 5 and 3 or not 7").produceAST())
 
 class PenguinScript {
