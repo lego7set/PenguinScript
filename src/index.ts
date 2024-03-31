3816015b4ea8797e689de9e948b179f2925629fb
lego7set <84934297+lego7set@users.noreply.github.com>
2024-03-31 11:33:51 -0400

    make extension work

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,94 @@ import Parser from "./parsing/parser";
 
 import Lexer from "./parsing/lexer";
 
-class PenguinScript {
-  getInfo() {
-    return {
-      id: "vgspenguinscript",
-      name: "PenguinScript",
-      blocks: []
-    }
-  }
-  getLexer() {
-    return Lexer;
-  }
-  getParser() {
-    return Parser
-  }
+import JSGenerator from "./transpile/jsGen";
+
+function transpile(code: string, warpTimer: boolean, isWarp: boolean): any {
+  const program = new Parser(code).produceAST();
+  const generator = new JSGenerator(program);
+  return generator.transpile("generator", true, warpTimer, isWarp)
 }
+
+function preCompile(code: string, warpTimer: boolean, isWarp: boolean): any {
+  const program = new Parser(code).produceAST();
+  const generator = new JSGenerator(program);
+  return "(yield* (function()*{" + generator.transpile("string", true, warpTimer, isWarp) + "})())";
+}
+
+import { SupportsExtensions, IsPenguinMod } from "./pmUtils/PenguinModDetector";
 let Scratch;
 // @ts-ignore
 if (typeof window === "object" && window && typeof window.document === "object" && typeof (Scratch = window.Scratch) === "object" && Scratch) {
   // Logic here
-  if (!Scratch.extensions.isPenguinMod) throw "Please load PenguinScript in PenguinMod";
-
+  if (!Scratch.extensions.isPenguinMod) throw "Please load PenguinScript in PenguinMod"; // i dnot need to explain tis
+  class PenguinScript {
+    constructor() {
+      Scratch.vm.runtime.registerCompiledExtensionBlocks("vgspenguinscript", this.getCompiledInfo());
+    }
+    getCompiledInfo() {
+      return {
+        ir: {
+          evalStack: (generator, block) => (generator.script.yields = true, {
+            kind: "stack",
+            code: generator.descendInputOfBlock("code", block)
+          })
+        },
+        js: {
+          evalStack: (node, compiler, imports) => {
+            const code = compiler.descendInput(node.code);
+            let preCompiled;
+            try {
+              const tryCompile = JSON.parse(code.asString());
+              preCompiled = preCompile(tryCompile, compiler.warpTimer, compiler.isWarp); // transpile at compile time to make it fast.
+              compiler.src += preCompiled + ";"
+            } catch(e) {
+              compiler.src += `(yield* transpile(${code.asString()}, ${compiler.warpTimer}, ${compiler.isWarp})());`
+            }
+          }
+        }
+      }
+    }
+    getInfo() {
+      return {
+        id: "vgspenguinscript",
+        name: "PenguinScript",
+        blocks: [
+          {
+            opcode: "evalStack",
+            blockType: Scratch.BlockType.COMMAND,
+            text: "evaluate [code]",
+            func: "noComp",
+            arguments: {
+              code: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "let x = 5; x = 6 xor 5;"
+              }
+            }
+          },
+        ]
+      }
+    }
+    noComp() {
+      throw "Please enable compiler."
+    }
+    transpile(code: string, warpTimer: boolean, isWarp: boolean) {
+      return transpile(code, warpTimer, isWarp);
+    }
+    getLexer() {
+      return Lexer;
+    }
+    getParser() {
+      return Parser
+    }
+  }
   Scratch.extensions.register(new PenguinScript());
 }
 
 module.exports = {
-  PenguinScript,
+  Lexer,
   Parser,
-  default: function transpile(src): string | Function {
-    //
-    return src;
-  }
+  JSGenerator,
+  default: transpile,
+  transpile
 }
           
