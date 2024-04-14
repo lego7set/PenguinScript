7e6acac72223c88121f9a34f45f94676d620c04a
lego7set <84934297+lego7set@users.noreply.github.com>
2024-04-14 08:08:31 -0400

    create loader

---
+++ src/runtime/loader.ts
@@ -0,0 +1,37 @@
+import { IsPenguinMod } from "../pmUtils/PenguinModDetector";
+import { _globalEnv } from "../transpile/jsGen";
+
+const trueEnv = _globalEnv.__env;
+
+class Loader {
+  public loadPackage(package) {
+    for (const globalIndex of package) {
+      const value = package[globalIndex];
+      trueEnv.set(globalIndex, {
+        get value() {return value},
+        set value() {throw new TypeError("Cannot overwrite built-in global")}
+      });
+    }
+  }
+  public loadPenguinModPackage(package) {
+    if (!IsPenguinMod) return;
+    for (const globalIndex of package) {
+      const value = package[globalIndex];
+      trueEnv.set(globalIndex, {
+        get value() {return value},
+        set value() {throw new TypeError("Cannot overwrite built-in global")}
+      });
+    }
+  }
+  public require(globalIndex) {
+    return trueEnv.get(globalIndex)?.value;
+  }
+  public requireScratch() {
+    // @ts-ignore
+    if (IsPenguinMod) return window.Scratch;
+    return null;
+  }
+}
+
+export default new Loader();
+
