b3b31d45081f71a83a5a9ff914784b71b72eb84e
lego7set <84934297+lego7set@users.noreply.github.com>
2024-04-15 19:46:25 -0400

    idk

--- src/runtime/debug.ts
+++ src/runtime/debug.ts
@@ -18,10 +18,10 @@ package.error = function* error(util, ...args) {
 package.EnterDebugMode = function debug(util, ...listOfThingsForAccess) {
   // do stuff.
   if (!window.confirm("Would you like to enter the PenguinScript debug mode? (requires advanced understanding of JS console to use)")) return false; // indicate not successful.
-  window.alert("Go into the console. The arguments passed into this function are accessible through the global variable PenguinScriptDebugMode.vars");
+  // window.alert("Go into the console. The arguments passed into this function are accessible through the global variable PenguinScriptDebugMode.vars");
   // @ts-ignore
   globalThis.PenguinScriptDebugMode = {__proto__: null, vars: listOfThingsForAccess, util};
-  debugger;
+  eval?.(`debugger;\n"This is the PenguinScript debug mode.";\n"Please read this entire thing.";\n"The arguments passed into the global EnterDebugMode function are accessible through the global variable PenguinScriptDebugMode.vars, and you can access variable items from PenguinScriptDebugMode.util";\n'To use this global variable, go into the "console" tab.'`); // indirect eval.
   // @ts-ignore
   delete globalThis.PenguinScriptDebugMode;
   return true;
