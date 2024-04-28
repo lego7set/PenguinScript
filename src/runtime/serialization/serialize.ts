dd7de406d00fce9e181130fa9ac6f3d0ab62f23e
lego7set <84934297+lego7set@users.noreply.github.com>
2024-04-28 09:22:58 -0400

    im going to make serialization later

---
+++ src/runtime/serialization/serialize.ts
@@ -0,0 +1,7 @@
+import { type } from "../structs";
+
+export default function Serialize() {
+  // Loop through structs and stuff
+  // Add a ref onto everything we encounter.
+  // Do not attempt to serialize sprites, and if sprites are encountered just ignore
+}
