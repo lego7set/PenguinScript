5061cdb7317e6e688e59a5445cd600dd7b10affc
lego7set <84934297+lego7set@users.noreply.github.com>
2024-04-28 08:58:29 -0400

    more functions & stuff so i can get this done

--- src/runtime/scratchmisc.ts
+++ src/runtime/scratchmisc.ts
@@ -25,13 +25,13 @@ package.broadcastAndWait = nativeFn(function* broadcastAndWait(util, message: an
   return started.length;
 }, false)
 
-package.getVariable(function* getVariableForAll(util, name: any) {
+package.getVariable = nativeFn(function* getVariableForAll(util, name: any) {
   const target = Scratch.vm.runtime.getTargetForStage();
   if (!(target instanceof Scratch.vm.exports.RenderedTarget)) throw new TypeError("Cannot get variable for a non-sprite");
   return target.lookupVariableByNameAndType(yield* toString(name), "", true)?.value ?? null;
 }, false)
 
-package.setVariable(function* setVariableForAll(util, name: any, value: any) {
+package.setVariable = nativeFn(function* setVariableForAll(util, name: any, value: any) {
   const target = Scratch.vm.runtime.getTargetForStage();
   if (!(target instanceof Scratch.vm.exports.RenderedTarget)) throw new TypeError("Cannot get variable for a non-sprite");
   const variable = target.lookupVariableByNameAndType(yield* toString(name), "", true);
@@ -51,8 +51,61 @@ function mouseClicked() {
   return Scratch.vm.runtime.ioDevices.mouse.getIsClicked();
 }
 
+package.isKeyDown = nativeFn(function* isKeyDown(util, key) {
+  if (!Scratch.vm.runtime.ioDevices.keyboard) return false;
+  return Scratch.vm.runtime.ioDevices.keyboard.getKeyIsDown(key);
+}, false)
+
+package.isKeyHit = nativeFn(function* isKeyHit(util, key) {
+  if (!Scratch.vm.runtime.ioDevices.keyboard) return false;
+  return Scratch.vm.runtime.ioDevices.keyboard.getKeyIsHit(key);
+}, false)
+
+export function* wait({timer, isStuck, isWarp}, ms) {
+  if (typeof ms !== "number" || Object.is(ms, NaN)) throw new TypeError("ms in global wait must be a number (and not NaN).");
+  ms = Math.max(0, ms);
+  const waitTimer = new timer();
+  waitTimer.start(); // start timer
+  while (waitTimer.timeElapsed() < ms) {
+    if (!isWarp || isStuck()) yield; // yield so thingy doesnt freeze
+  }
+  return true; // so that you can do while global wait(10) doSomething
+}
+
+export function* waitUntil(util, conditionFunction, ...argFuncs) {
+  if (typeof conditionFunction !== "function") throw new TypeError("The first arg to global waitUntil must be a function.");
+  for (const func of argFuncs) if (typeof func !== "function") throw new TypeError("All args after the first arg must be functions");
+  while (yield* wait(util, 4)) { // prevent freezing
+    // evaluate args.
+    const args = argFuncs.map(func => func(util)); // i almost forgor that all funcs take in the util object.
+    if ((conditionFunction(util, ...args) ?? false) === false) break;
+  }
+  return true; // why would i return false if the condition is true?
+}
+
+package.wait = nativeFn(wait, false)
+package.waitUntil = nativeFn(waitUntil, false)
+
+package.setTempVar = nativeFn(function* setTempVar(util, name, v) {
+  return util.tempVars[String(name)] = v;
+}, false)
+
+package.getTempVar = nativeFn(function* getTempVar(util, name, v) {
+  return util.tempVars[String(name)];
+}, false)
+
+package.tempVarExists = nativeFn(function* tempVarExists(util, name, v) {
+  return !!util.tempVars[String(name)]; // this is literally what the pm compiler uses
+}, false)
+
+let startTime = self.performance.now();
+package.timeSinceBlueFlag = nativeFn(function* timeSinceBlueFlag() {
+  return self.performance.now() - startTime;
+}, false)
+
 package.onLoad = function() {
   Scratch = this.requireScratch();
+  Scratch.vm.runtime.on("PROJECT_START", () => startTime = self.performance.now());
   this.loadRaw("mouseDown", {
     get value() {return mouseDown()},
     set value(v) {throw new TypeError("Cannot overwrite mouseDown global value")}
