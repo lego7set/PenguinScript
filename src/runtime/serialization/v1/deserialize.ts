2039ccf6bba94d0ae60e55b95925678daac327df
lego7set <84934297+lego7set@users.noreply.github.com>
2024-04-30 11:33:19 -0400

    complete later

--- src/runtime/serialization/v1/deserialize.ts
+++ src/runtime/serialization/v1/deserialize.ts
@@ -69,3 +69,86 @@ class Lexer { // the deserializer lexer. im going to use regexp cuz its faster a
     throw new Error(`Unexpected character at position ${this.position}`);
   }
 }
+
+class SeenMap {
+  private map: Record<string, any> = {};
+  private awaitedRefs: Record<string, (v)=>void> = {};
+  seen(ref) {
+    return this.map[ref] !== undefined;
+  }
+  get(ref) {
+    return this.map[ref];
+  }
+  add(ref, obj) {
+    if (this.awaitedRefs[ref]) {
+      this.awaitedRefs[ref](obj);
+      delete this.awaitedRefs[ref];
+    }
+    return this.map[ref] = obj;
+  }
+  await(ref, callback) {
+    this.awaitedRefs[ref] = callback;
+  }
+}
+
+class Transformer { // convert tokens into structs
+  protected tokens: Token[];
+  public transformed: any;
+  constructor(tks: Token[]) {
+    this.tks = tokens;
+    // process tokens here
+  }
+  protected transformRecursive(seen = new SeenMap(), current: {obj: any, add: (v) => void} | null = null): any {
+    if (!this.not_eof()) return;
+    switch (this.at().type) {
+      case TokenType.STRING: {
+        // simply just json.parse this thingy
+        return JSON.parse(this.eat().raw);
+      }
+      case TokenType.NUMBER: {
+        const raw = this.eat().raw;
+        if (raw === "inf") return Infinity;
+        if (raw === "-inf") return -Infinity;
+        if (raw === "NaN") return NaN;
+        try {
+          return JSON.parse(raw) as unknown as number;
+        } catch(e) {
+          throw new SyntaxError("Expected number, got invalid number instead.")
+        }
+      }
+      case TokenType.TRUE: {
+        return true;
+      }
+      case TokenType.FALSE: {
+        return false;
+      }
+      case TokenType.NULL: {
+        return null;
+      }
+      case TokenType.REF: {
+        if (!current) throw new TypeError("Refs may only appear from within objects, arrays, and structs.")
+        if (seen.seen(this.at().raw)) return seen.get(this.at().raw);
+        // idk if this will ever happen for invalid refs? everything is l -> r
+        seen.await(this.at().raw, current.add);
+      }
+    }
+  }
+  protected not_eof() {
+    return this.tokens[0] && this.tokens[0].type !== TokenType.EOF;
+  }
+  protected eat() {
+    return this.tokens.shift() as unknown as Token;
+  }
+  protected at() {
+    return this.tokens[0] as unknown as Token
+  }
+  protected optional(...types: TokenType[]) {
+    if (!this.not_eof()) return null;
+    if (types.indexOf(this.at().type) !== -1) return this.eat();
+    return null;
+  }
+  protected expect(...types: TokenType[]) {
+    if (types.indexOf(this.at().type) === -1) throw new SyntaxError(`Expected token types ${types.map(v=>TokenType[v]).join(", ")}, but got ${TokenType[this.at().type]} instead.`)
+    return this.eat();
+  }
+}
