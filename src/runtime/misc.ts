1d9bec81c0bd645ad12bc7a894c593c326a04ca5
lego7set <84934297+lego7set@users.noreply.github.com>
2024-05-03 15:57:26 -0400

    e

--- src/runtime/misc.ts
+++ src/runtime/misc.ts
@@ -1,9 +1,9 @@
 import nativeFn from "./internal/nativefunc";
 
-const package = { __proto__: null };
+const pkg = { __proto__: null };
 
-package.exit = nativeFn(function* exit(util, value: any) {
+pkg.exit = nativeFn(function* exit(util, value: any) {
   throw {isExit: true, returnValue: value}; // exits
 }, false)
 
-export default package;
+export default pkg;
