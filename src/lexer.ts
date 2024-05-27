12d605c0886f902b1ea5970970587a0c7a09b9d6
lego7set <84934297+lego7set@users.noreply.github.com>
2024-05-27 14:16:22 -0400

    binary

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,6 +1,6 @@
 class Lexer {
   static tokenRegexps: Record<string, RegExp> = {
     identifierKeyword: /^[a-zA-Z_]+[a-ZA-Z0-9_]*/,
-    number: /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+/
+    number: /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/
   }
 }
