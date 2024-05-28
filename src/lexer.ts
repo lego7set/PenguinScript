5e1ddf7fe8c1dcaf33170ea172971d9500e09f80
lego7set <84934297+lego7set@users.noreply.github.com>
2024-05-28 11:32:59 -0400

    brackets

--- src/lexer.ts
+++ src/lexer.ts
@@ -3,5 +3,7 @@ class Lexer {
     identifierKeyword: /^[a-zA-Z_]+[a-ZA-Z0-9_]*/,
     number: /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/,
     string: /^"((?<=\\)"|[^"])*(?<!\\)"/, // this is the worst regexp ive had to think of yet
+    open_thingy: /^[{([]/,
+    close_thingy: /^[})\]]/
   }
 }
