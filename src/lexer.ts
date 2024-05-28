d08622f88d35d687d5bac766735ffb9c91a9ce7a
lego7set <84934297+lego7set@users.noreply.github.com>
2024-05-28 14:10:23 -0400

    changes

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,9 +1,14 @@
 class Lexer {
-  static tokenRegexps: Record<string, RegExp> = {
-    identifierKeyword: /^[a-zA-Z_]+[a-ZA-Z0-9_]*/,
-    number: /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/,
-    string: /^"((?<=\\)"|[^"])*(?<!\\)"/, // this is the worst regexp ive had to think of yet
-    open_thingy: /^[{([]/,
-    close_thingy: /^[})\]]/
-  }
+  static tokenRegexps: [string, RegExp][] = [
+    ["identifierKeyword", /^[a-zA-Z_]+[a-ZA-Z0-9_]*/],
+    ["number", /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/],
+    ["string", /^"((?<=\\)"|[^"])*(?<!\\)"/], // this is the worst regexp ive had to think of yet
+    ["open_thingy", /^[{([]/],
+    ["close_thingy", /^[})\]]/],
+    ["increment", /^(++|--)/],
+    ["binary_operator", /^([-+*/%^<>]|==|!=)/],
+    ["assignment", /^(=|+=|-=|*=|\/=|%=|^=|?=)/],
+    ["question_mark", /^\?/],
+    ["colon", /^:/]
+  ]
 }
