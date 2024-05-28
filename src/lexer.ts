bb78c8644e73172c2a1cef76a6db718196393fb5
lego7set <84934297+lego7set@users.noreply.github.com>
2024-05-28 11:31:14 -0400

    string regexp

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,6 +1,7 @@
 class Lexer {
   static tokenRegexps: Record<string, RegExp> = {
     identifierKeyword: /^[a-zA-Z_]+[a-ZA-Z0-9_]*/,
-    number: /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/
+    number: /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/,
+    string: /^"((?<=\\)"|[^"])*(?<!\\)"/, // this is the worst regexp ive had to think of yet
   }
 }
