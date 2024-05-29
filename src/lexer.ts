e735ae1a5c43d25ff565bba31e4a80ef6dbd92a1
lego7set <84934297+lego7set@users.noreply.github.com>
2024-05-29 09:09:32 -0400

    just realized that the function that calls this has to be async cuz async features

--- src/lexer.ts
+++ src/lexer.ts
@@ -14,9 +14,9 @@ class Token {
 }
 
 class Lexer {
-  static source: string = "";
-  static position: number = 0;
-  static tokenRegexps: [string, RegExp][] = [
+  source: string = "";
+  position: number = 0;
+  tokenRegexps: [string, RegExp][] = [
     ["identifierKeyword", /^[a-zA-Z_]+[a-ZA-Z0-9_]*/],
     ["number", /^([0-9]+\.?[0-9]*(e[+-]?[0-9]+)?|\.[0-9]+(e[+-]?[0-9]+)?)|0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+/],
     ["string", /^"((?<=\\)"|[^"])*(?<!\\)"/], // this is the worst regexp ive had to think of yet
@@ -28,7 +28,7 @@ class Lexer {
     ["question_mark", /^\?/],
     ["colon", /^:/]
   ]
-  static tokenHandlers: Record<string, (this: typeof Lexer, matched: string) => Token> = {
+  tokenHandlers: Record<string, (this: typeof Lexer, matched: string) => Token> = {
     
   }
 }
